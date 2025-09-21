82722cc31a8a5b70f77eeccfb834736a2f70480d
Aliza Khan <alizaadi2@gmail.com>
2025-09-22 00:25:22 +0500

    Added backend deployed URL in BASEURL

--- src/config/axiosInstance.js
+++ src/config/axiosInstance.js
@@ -1,15 +1,25 @@
 import axios from "axios"
 
-const BaseURL = "https://realestate-properties-backend.onrender.com/auth"
-const BaseURLProducts = "https://realestate-properties-backend.onrender.com/properties"
-// https://realestate-properties-backend.onrender.com
-// http://localhost:8000/
+// const BaseURL = "https://realestate-properties-backend.onrender.com/auth"
+// const BaseURLProducts = "https://realestate-properties-backend.onrender.com/properties"
+// // https://realestate-properties-backend.onrender.com
+// // http://localhost:8000/
+
+// export const axiosInstance = axios.create({
+//     baseURL: BaseURL
+// })
+
+
+// export const axiosInstanceProperty = axios.create({
+//     baseURL: BaseURLProducts
+// })
+
+const API = "https://realestate-properties-backend.onrender.com";
 
 export const axiosInstance = axios.create({
-    baseURL: BaseURL
-})
-
+  baseURL: `${API}/auth`
+});
 
 export const axiosInstanceProperty = axios.create({
-    baseURL: BaseURLProducts
-})
+  baseURL: `${API}/properties`
+});
